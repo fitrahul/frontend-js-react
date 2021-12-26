6c53e056de0f8703a3ef18fdfde4b952dcee63b9
Rahul <rahulee19@gmail.com>
2021-12-26 10:19:07 +0530

    infinite scroll

---
+++ infinite_scroll/i_scroll.js
@@ -0,0 +1,22 @@
+    // 1. simple main function =>
+    var count = 1;
+    var parent = document.getElementById("main_div");
+    var main = () => {
+        for (let i = 1; i <= 25; i++) {
+            var name = `Masai school student ${count++}`
+            var child = document.createElement("p");
+            child.setAttribute("id", "child_div")
+            child.innerHTML = name;
+            parent.append(child);
+        }
+    }
+    main();
+
+    // 2. scrolling effect =>
+    parent.addEventListener("scroll", () => {
+        var { scrollTop, scrollHeight, clientHeight } = document.getElementById("main_div");
+        console.log({ scrollTop, scrollHeight, clientHeight });
+        if (clientHeight + scrollTop >= scrollHeight - 5) {
+            main();
+        }
+    })
